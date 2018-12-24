cc3524e0180ff32f7aae5dcafc36f05e863442ea
Charles Clemens <cclemensjr@gmail.com>
2018-12-24 00:11:56 -0800

    Completed function for displaying the max result

--- Prework-CodeChallenges/Program.cs
+++ Prework-CodeChallenges/Program.cs
@@ -6,7 +6,23 @@ namespace Prework_CodeChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[] { 6, 4, 4, 1, 3 };
+            int chosen = 4;
+            getArrayMaxResult(arr, chosen);
+            Console.ReadLine();
+        }
+
+        
+        static void getArrayMaxResult(int[] numArray, int selection)
+        {
+            int timesInArray = 0;
+
+           foreach(var num in numArray)
+           {
+                if (num == selection) timesInArray++;
+           }
+
+           Console.WriteLine(selection * timesInArray);
         }
     }
 }
